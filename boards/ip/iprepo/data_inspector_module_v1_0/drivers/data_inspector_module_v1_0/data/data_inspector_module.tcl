88826b7a39399131f0db7ef5e632a85cc0c6b37d
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-05-12 13:33:08 +0100

    Remove old data_inspector_module IP Core

--- boards/ip/iprepo/data_inspector_module_v1_0/drivers/data_inspector_module_v1_0/data/data_inspector_module.tcl
+++
@@ -1,6 +0,0 @@
-proc generate {drv_handle} {
-    xdefine_include_file $drv_handle "xparameters.h" "data_inspector_module" "NUM_INSTANCES" "DEVICE_ID" "C_DATA_INSPECTOR_MODULE_S_AXI_BASEADDR" "C_DATA_INSPECTOR_MODULE_S_AXI_HIGHADDR" 
-    xdefine_config_file $drv_handle "data_inspector_module_g.c" "data_inspector_module" "DEVICE_ID" "C_DATA_INSPECTOR_MODULE_S_AXI_BASEADDR" 
-    xdefine_canonical_xpars $drv_handle "xparameters.h" "data_inspector_module" "DEVICE_ID" "C_DATA_INSPECTOR_MODULE_S_AXI_BASEADDR" "C_DATA_INSPECTOR_MODULE_S_AXI_HIGHADDR" 
-
-}
